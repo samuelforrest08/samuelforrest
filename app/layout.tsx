723c8b7eb57be56e8902b7124209bcc8dc0dbc3d
Samuel Forrest <samuel@samuelforrest.co.uk>
2026-05-16 15:17:41 +0100

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import type { Metadata, Viewport } from "next";
-import { Inter, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const viewport: Viewport = {
@@ -91,11 +91,6 @@ const inter = Inter({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -159,7 +154,7 @@ export default function RootLayout({
         />
       </head>
       <body
-        className={`${inter.variable} ${geistMono.variable} bg-zinc-950 tracking-tight antialiased`}
+        className={`${inter.variable} bg-zinc-950 tracking-tight antialiased`}
       >
         <div className="flex min-h-screen w-full flex-col font-(family-name:--font-inter-tight)">
           <div className="relative mx-auto w-full max-w-screen-sm flex-1 px-4 pt-20">
