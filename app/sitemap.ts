430581d4c8590c6e25f0e05ffaf36626454a0353
Samuel Forrest <samuel@samuelforrest.co.uk>
2026-05-16 13:02:05 +0100

    changes

--- app/sitemap.ts
+++
@@ -1,9 +0,0 @@
-import { MetadataRoute } from "next";
-
-const base = "https://samuelforrest.me";
-
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    { url: base, lastModified: new Date(), changeFrequency: "weekly", priority: 1 },
-  ];
-}
